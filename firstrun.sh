c24eb72ce5155b97cdaea74c1aa7a8e6ed9de50e
hernandito <archi@inch.com>
2015-04-30 15:09:09 -0400

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -3,10 +3,12 @@
 # Check if config exists. If not, copy in the sample config
 if [ -f /opt/calibre/calibre-server ]; then
   echo "Using existing install."
+  /opt/calibre/calibre-server --with-library /config
 else
   echo "Installing Clean - Apply any Custom Edits"
   cd /opt
   wget --no-check-certificate -nv -O- https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py | python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main('/opt/', True)"
   cp -ar /downloads/calibre-edits/calibre/resources/content_server /opt/calibre/resources/
+  /opt/calibre/calibre-server --with-library /config
 fi
 
