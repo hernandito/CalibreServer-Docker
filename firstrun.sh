39e27d627b2c25ac0eb08d3cc368772c7bed3239
hernandito <archi@inch.com>
2015-04-30 14:54:24 -0400

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -7,6 +7,6 @@ else
   echo "Installing Clean - Apply any Custom Edits"
   cd /opt
   wget --no-check-certificate -nv -O- https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py | python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main('/opt/', True)"
-  cp -ar /downloads/calibre-edits/calibre/resources /opt/calibre/
+  cp -ar /downloads/calibre-edits/calibre/resources/content_server /opt/calibre/resources/
 fi
 
